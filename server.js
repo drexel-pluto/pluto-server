b01af32aad491707f8c4f3fe10685675359f15d4
Mike Carbone <mfcbone2@gmail.com>
2020-02-14 16:05:45 -0500

    :fire: Boosts image max file size

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client/')));
 app.use(fileParser({
 	rawBodyOptions: {
-			limit: '2mb',  // Image file size limit
+			limit: '100mb',  // Image file size limit
 	}
 }));
 
--- services/ImageService.js
+++ services/ImageService.js
@@ -22,7 +22,7 @@ const Multer = require('multer');
 const multer = Multer({
     storage: Multer.MemoryStorage,
     limits: {
-        fileSize: 2 * 1024 * 1024 // no larger than 5mb
+        fileSize: 100 * 1024 * 1024 // no larger than 5mb
     }
 });
 
