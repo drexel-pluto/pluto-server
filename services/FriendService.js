66bd05a6781eab84932f35c7c47d78f21bb90a0f
Mike Carbone <mfcbone2@gmail.com>
2020-03-13 13:24:11 -0400

    :bug: Further debugging

--- services/FriendService.js
+++ services/FriendService.js
@@ -113,7 +113,7 @@ module.exports = () => {
             return friendSet.has(possibleFriendID.toString());
         },
         async ensureFriends(userObj, possibleFriendID, _params) {
-            console.log(_params);
+            console.log('ensureFriends: ', userObj._id, possibleFriendID);
             // Pass if user is self
             if (userObj._id == possibleFriendID) { return }
             const friends = userObj.friendIds;
